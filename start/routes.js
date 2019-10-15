a497c80c5c9c5be11f8a98453acf60d875db2789
alfin1998 <alfinyuki@gmail.com>
2019-10-15 08:41:19 +0700

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('template')
 
 Route.group(() => {
     Route.get('crawling', 'TweetController.index')
